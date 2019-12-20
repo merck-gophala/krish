48eea60ff20d0ff323fd4c55cddbd3ba9e0f32a9
P Gophala Krishnan <pkrishna@global.sial.com>
2019-12-20 19:01:38 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+
+
+function App() {
+  return (
+    <div className="App">
+       {/* <Header /> */}
+       <h2>Hello World...!!</h2>
+       <h3>Welcome to React App..!!</h3>
+    </div>
+  );
+}
+
+export default App;
